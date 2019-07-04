31a19e2c6d921ebdbeaf8306639b324e0c53fce1
wuyongli <wuyongli8887@163.com>
2019-07-04 20:55:45 +0800

    父子组件传值

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>这是写在app.vue内的h1标签</h1>
+    <HelloCode v-bind:books="codebooks" @pfn="sayHi" @pfn2="test2"></HelloCode>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloCode from "./components/HelloCode.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloCode
+  },
+  data: function() {
+    return {
+      codebooks: { name: "python", price: 80 }
+    };
+  },
+  methods: {
+    sayHi: function(data) {
+      alert(data);
+    },
+    test2() {
+      alert("$emit的第二种写法");
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: red;
 }
 </style>
---
+++ src/components/HelloCode.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{books.name}}</h1>
+    <h3>{{books.price}}</h3>
+    <button v-on:click="sfn">click</button>
+    <button @click="$emit('pfn2')">2click</button>
+    <code>
+        第一种写法：
+      1、在父组件中定义方法 pfn
+      2、在子组件 组件引入标签 中绑定自定义事件 v-on:自定义事件名="父组件中的方法" ==> @pfn="parentFn"
+      3、子组件中通过$emit()触发自定义事件事件 this.$emit(pfn,参数列表。。。)
+      <br/>>第二种写法：
+      在子组件中，v-on:事件名="$emit('父组件名')"
+    </code>
+  </div>
+</template>
+<script>
+export default {
+  props: ["books"],
+  methods:{
+      sfn(){
+          this.$emit('pfn','我是子组件的传值')
+      }
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- 写上scoped属性，则css只对当前组件有效 -->
+<style scoped>
+h1 {
+  color: chocolate;
+}
+</style>
+
+
+
+
